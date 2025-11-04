ffaf37008f7182387f78b691b310eb1a274fdf6b
Eiyron <164208802+gnoweee@users.noreply.github.com>
2025-11-04 23:25:51 +0800

    initial SNACC 'N BITE program
    SNACC 'N BITE code

---
+++ SNACC_N_BITE.cpp
@@ -0,0 +1,9 @@
+#input <iostream>
+
+using namespace std;
+int main() {
+cout << "===========================\n";
+cout << "WELCOME TO SNACC 'N BITE\n"; 
+cout << "===========================\n";
+
+}

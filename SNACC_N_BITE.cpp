e070ece038ebe43165155f491290b3b8c49ecb04
Eiyron <164208802+gnoweee@users.noreply.github.com>
2025-11-16 21:21:46 +0800

    Fix typos and improve user prompts in SNACC_N_BITE

--- SNACC_N_BITE.cpp
+++ SNACC_N_BITE.cpp
@@ -1,4 +1,4 @@
-#input <iostream>
+#include <iostream>
 
 using namespace std;
 int main() {
@@ -34,29 +34,89 @@ cout << "===========================\n";
   "EXTRA Fries"
   };
 
-  int quatity[11] = {0};
+  int quantity[11] = {0};
   double price[11] = {79, 99, 49, 69, 79, 25, 20, 15, 15, 10, 25};
-  int choice;
+  int choice, qty;
+  char again = 'Y';
   
+
+// Ordering Section
   do {
-    cout << "\n Enter the number of item you want to order: ";
+    cout << "\nEnter the number of item you want to order: ";
     cin >> choice;
 
     if (cin.fail() || choice < 1 || choice > 11 ) {
       cin.clear();
       cin.ignore (1000, '\n');
-      cout << "Invalid Choice! Please Try Another. \n";
+      cout << "Invalid Choice! Please Try Another.\n";
       continue;
     }
-  }
     cout << "Enter Quantity: ";
-  cin >> choice;
+  cin >> qty;
   if (cin.fail() || qty <= 0) {
     cin.clear();
     cin.ignore(1000, '\n');
-    cout << "Invalid Quantity! Please Enter a positive number. \n;
+    cout << "Invalid Quantity! Please Enter a positive number.\n" ;
       continue;
   }
   quantity[choice - 1] += qty;
 
+  cout << "Do you want to Add another Item? (Y/N):" ;
+  cin >> again;
+
+      
+    // View menu again option
+    if (again == 'Y' || again == 'y') {
+        cout << " \nWould you like to see the menu again? (Y/N): ";
+        char viewMenu;
+        cin >> viewMenu; 
+        if (viewMenu == 'Y' || viewMenu == 'y') {
+             cout << "\n===== MAIN MENU =====\n";
+    cout << "1. Shawarma Rice (Regular) - ₱79\n";
+    cout << "2. Shawarma Rice (W/ Fries) - ₱99\n";
+    cout << "3. Burger Patty W/ Rice (Regular) - ₱49\n";
+    cout << "4. Burger Patty W/ Rice (W/ Fries) - ₱69\n";
+    cout << "5. Double Patty W/ Rice (Regular) - ₱79\n";
+    cout << "\n===== ADD ONS =====\n";
+    cout << "6. EXTRA Meat - ₱25\n";
+    cout << "7. EXTRA Java Rice - ₱20\n";
+    cout << "8. EXTRA Garlic Mayo - ₱15\n";
+    cout << "9. EXTRA Melted Cheese - ₱15\n";
+    cout << "10. EXTRA Veggies - ₱10\n";
+    cout << "11. EXTRA Fries - ₱25\n";
+        }
+    }
+  } while (again == 'Y' || 'y');
+
+// Edit section
+    char editChoice = 'Y';
+    while (editChoice == 'Y' || editChoice == 'y') {
+        cout << "\nDo you want to remove or change an item  before finalizing? (Y/N): ";
+        cin >> editChoice;
+        if (editChoice == 'Y' || editChoice == 'y') {
+            cout <<"\nEnter the item you want to change or remove: ";
+            int removeItem;
+            cin >> removeItem;
+
+        if (removeItem >= 1 && removeItem <= 11 && quantity[removeItem - 1] > 0 ) {
+            cout << "Current quantity for" << items[removeItem - 1] << ":" << quantity[removeItem - 1] << endl;
+            cout << "Enter new quantity (0 to remove completely): ";
+            int newQty;
+            cin >> newQty;
+            
+            if (newQty >= 0) {
+                quantity[removeItem - 1] = newQty;
+                cout << "Item updated successfully!\n";
+                } else {
+                  cout << "Invalid quantity!\n";
+                }
+        } else {
+            cout << "Invalid item number or item not yet ordered.\n" ;
+        }
+        }
+    }
+
+
+    
+    return 0;
 }

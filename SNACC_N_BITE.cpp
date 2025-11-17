d4027be60de2bb0546182e356d073436a4aa49c3
Eiyron <164208802+gnoweee@users.noreply.github.com>
2025-11-17 09:53:07 +0800

    Enhance receipt with tax and discount calculations
    Added discount message and receipt formatting.

--- SNACC_N_BITE.cpp
+++ SNACC_N_BITE.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 int main() {
 cout << "===========================\n";
 cout << "WELCOME TO SNACC 'N BITE\n"; 
 cout << "===========================\n";
 
+cout << "\n GET 20% DISCOUNT IF YOUR ORDER IS ₱500 OR ABOVE! \n";
+    
     cout << "\n===== MAIN MENU =====\n";
     cout << "1. Shawarma Rice (Regular) - ₱79\n";
     cout << "2. Shawarma Rice (W/ Fries) - ₱99\n";
@@ -86,7 +88,7 @@ cout << "===========================\n";
     cout << "11. EXTRA Fries - ₱25\n";
         }
     }
-  } while (again == 'Y' || 'y');
+  } while (again == 'Y' || again == 'y');
 
 // Edit section
     char editChoice = 'Y';
@@ -99,7 +101,7 @@ cout << "===========================\n";
             cin >> removeItem;
 
         if (removeItem >= 1 && removeItem <= 11 && quantity[removeItem - 1] > 0 ) {
-            cout << "Current quantity for" << items[removeItem - 1] << ":" << quantity[removeItem - 1] << endl;
+            cout << "Current quantity for " << items[removeItem - 1] << ":" << quantity[removeItem - 1] << endl;
             cout << "Enter new quantity (0 to remove completely): ";
             int newQty;
             cin >> newQty;
@@ -115,8 +117,43 @@ cout << "===========================\n";
         }
         }
     }
+// Receipt section
+    cout << fixed << setprecision(2);
+    cout << "\n=============================================\n";
+    cout << "                   RECEIPT\n";
+    cout << "=============================================\n";
+    cout  << left << setw(30) << "Item" << setw(10) << "Qty" << "Subtotal\n";
+    cout << "---------------------------------------------\n";
 
+    double total = 0;
+    for (int i = 0; i < 11; i++) {
+        if (quantity[i] > 0) {
+        double subtotal = price[i] * quantity[i];
+     cout << left << setw(30) << items[i]
+     << setw(10) << quantity [i]
+     << "₱" << subtotal << endl;
+            total += subtotal;
+        }
+    }
+    cout << "---------------------------------------------\n";
+    cout << right << setw(35) << "TOTAL: ₱" << total << endl;
 
-    
+ // tax/discount section
+double taxRate = 0.12;
+double discountRate = 0.20;
+double tax = total * taxRate; 
+double discount = 0;
+
+if (total >= 500) {
+discount = total * discountRate;
+}
+double finalTotal = total + tax - discount;
+cout << "Tax (12%): ₱" << tax << endl;
+    if (discount > 0)
+        cout << "Discount (20%): -₱" << discount << endl;
+    cout << "---------------------------------------------\n";
+    cout << right << setw(35) << "FINAL TOTAL: ₱" << finalTotal << endl;
+    cout << "---------------------------------------------\n";
+
     return 0;
 }
